81cdf49a6646eb0bf9800026c66b6f477dc7fe2a
yotsuba1022 <yotsuba1022@gmail.com>
2017-07-25 22:31:15 +0800

    Initial commit.

---
+++ src/main/java/uruz7/commons/util/App.java
@@ -0,0 +1,13 @@
+package uruz7.commons.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
